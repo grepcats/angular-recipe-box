52365ec92811883862e1234f74cceebab8851b26
Kayla Ondracek and Masen Matthews <kayla@>
2018-03-27 11:58:41 -0700

    start on sidebar styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import { Recipe } from './models/recipe.model';
 export class AppComponent {
   title = 'Recipe Box';
 
-  masterRecipeList: Recipe[] = [new Recipe('Oatmeal', 'oats, water, salt', 'Boil the oats in water and salt'),
+  masterRecipeList: Recipe[] = [new Recipe('Oatmeal', 'oats, water, salt' , 'Boil the oats in water and salt'),
   new Recipe('Kale Chips', 'kale, salt, olive oil, pepper', 'Set oven to 400 degrees. Tear kale into pieces. Drizzle with olive oil and add seasonings. Bake for 10-12 minutes'),
   new Recipe('Coffee', 'coffee beans, water, whipping cream', 'Grind coffee beans to desired consistency. Put grounds in French Press and pour water over them. Figure out the rest')];
   selectedRecipe: Recipe = null;
